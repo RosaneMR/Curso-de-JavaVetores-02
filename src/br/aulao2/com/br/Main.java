51c8fce3f90adcf9c94a8b43d296785c76054a2b
Rosane Maciel <rosanemarciel@gmail.com>
2022-06-13 15:03:19 -0300

    frist commit

---
+++ src/br/aulao2/com/br/Main.java
@@ -0,0 +1,14 @@
+package br.aulao2.com.br;
+     public class Main{
+    public static void main(String[]args){
+        String mes[]={"JAN","FEV","MAR","ABR","MAI","JUN",
+                "JUL","AGO","SET","OUT","NOV","DEZ"};
+        int tot[] ={31,28,31,30,31,30,31,31,30,31,30,31};
+        for(int c=0; c< mes.length; c++ ){
+            //  Object tem;
+            System.out.println("O mes de" + mes[c] + " tem " +
+                    tot[c]+ " dias ao todo.");{
+            }
+        }
+
+    }
